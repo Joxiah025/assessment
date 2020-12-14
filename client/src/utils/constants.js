3ef08a0b194a79190bd1057911a4d837d286d870
joxiah025 <creativejosiah@gmail.com>
2020-12-14 17:44:20 +0100

    cors hack

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,9 +1,2 @@
 export const base_url = "https://assessment-laravel.herokuapp.com/api/";
-export const header = {
-    mode: 'no-cors',
-    headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    credentials: 'same-origin'
-};
+export const header = { crossdomain: true };
